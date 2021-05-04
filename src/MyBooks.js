2b91593e3fffccb84342be7155f05b7374576e83
Alexander Nederegger <an@Alexanders-MacBook-Pro.local>
2021-05-04 19:03:36 +0200

    Set up the project. Implemented the react ROUTER architecture with the base URL and the /search URL. WBesides the App.js component (wheose state decides which view is rendered) the project will be split in two other components. First the 'MyBooks' component which will display all the books in my reading list respective of ttheir category. Second the 'SearchBooks' component which will be an alternative view where the user can search for new books by title or author to add to the list. The  raw files for both components are set up such that one can toggle between the views by manually changing the URL of the browser from '/' to '/search'. Next up is the development of both components and their interactive functionality.

---
+++ src/MyBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class MyBooks extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <h1>This is my bookshelf</h1>
+        )
+    }
+};
+
+export default MyBooks;
---
+++ src/SearchBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SearchBooks extends Component {
+    state = {
+
+    }
+
+    render() {
+        return(
+            <h2>This is my search view</h2>
+        )
+    }
+};
+
+export default SearchBooks;
